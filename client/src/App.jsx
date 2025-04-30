254dd466e68e28956225548bd0597522afbac6d0
TriUbi <triubilla@gmail.com>
2025-04-30 11:47:20 +0200

    added admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import NotFound from "./pages/NotFound";
 import FormPage from "./pages/FormPage";
+import Admin from "./pages/AdminPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
       <Route path="/form" element={<FormPage />} />
+      <Route path="/admin" element={<Admin />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormPage = () => {
+  return (
+    <main>
+      <h2>Admin Page!</h2>
+    </main>
+  );
+};
+
+export default FormPage;
