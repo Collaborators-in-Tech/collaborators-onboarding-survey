52dd3ee5556267681f4392d929c0e0a10899753e
filzasaleem <filza.saleem05@gmail.com>
2025-05-22 22:08:04 +0200

    added description field to the question table

--- server/database/seeders/QuestionsSeeder.php
+++ server/database/seeders/QuestionsSeeder.php
@@ -17,6 +17,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Ange Ditt fullständiga namn',
+                'description' => null,
                 'type' => 'text',
                 'is_required' => true,
                 'sort_order' => 1,
@@ -27,6 +28,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Vilken yerkesroll alignar med dig just nu, eller vilken roll strävar du arr uppnå?',
+                'description' => null,
                 'type' => 'text',
                 'is_required' => true,
                 'sort_order' => 2,
@@ -37,6 +39,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Vad är det stärsta behovet du har just nu i din karriärutveckling?',
+                'description' => 'Behöver du mer erfarenhet av projektarbete, teknisk, utveckling,coaching,intervjuträning eller nått annat?',
                 'type' => 'text',
                 'is_required' => true,
                 'sort_order' => 3,
@@ -47,6 +50,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Vad hoppas du få ut av att vara en del av Collaborators?',
+                'description' => null,
                 'type' => 'text',
                 'is_required' => true,
                 'sort_order' => 4,
@@ -58,6 +62,8 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Vad ser du som ditt främsta syfte av att vara en del av Collaborators?',
+                'description' => 'Vill du nätverka och få inspiration genom att komma i kontakt 
+                                    med andra inom branschen, eller föredrar du att vara med och bygga projekt för att utveckla dina färdigheter och erfarenheter?',
                 'type' => 'checkbox',
                 'is_required' => true,
                 'sort_order' => 5,
@@ -68,6 +74,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Vilka tider passar dig bäst för att delta i projekten just nu?',
+                'description' => null,
                 'type' => 'checkbox',
                 'is_required' => true,
                 'sort_order' => 6,
@@ -78,6 +85,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Hur många timmar kan du bidra med i projekten',
+                'description' => null,
                 'type' => 'checkbox',
                 'is_required' => true,
                 'sort_order' => 7,
@@ -88,6 +96,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Beskriva dig själv med emojis :)',
+                'description' => null,
                 'type' => 'text',
                 'is_required' => false,
                 'sort_order' => 8,
@@ -98,6 +107,8 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Har du några andra önskemål, idéer om hur vi kan förbättra gruppens möjligheter, eller feedback du vill dela med dig av?',
+                'description' => 'Det kommer komma en ordentlig onboarding och en plattform där all information kommer
+                                    att vara tillgänglig.',
                 'type' => 'text',
                 'is_required' => false,
                 'sort_order' => 9,
@@ -108,6 +119,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Ange din e-postaddress',
+                'description' => null,
                 'type' => 'text',
                 'is_required' => true,
                 'sort_order' => 10,
@@ -118,6 +130,7 @@ class QuestionsSeeder extends Seeder
             [
                 'form_id' => 1,
                 'question_text' => 'Do you agree to our terms and conditions?',
+                'description' => null,
                 'type' => 'boolean',
                 'is_required' => true,
                 'sort_order' => 11,
